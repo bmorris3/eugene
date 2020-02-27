bb4b0d12595ff3995a32a25408e6deee6693cca5
Brett Morris <brettmorris21@gmail.com>
2020-02-27 06:04:00 +0100

    numba jit implementation

--- eugene/tests/test_core.py
+++ eugene/tests/test_core.py
@@ -3,18 +3,28 @@ import numpy as np
 
 from ..core import simulate_outbreak
 
+control_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                            os.pardir, 'data',
+                                            'control.txt'))
+
 
 def test_simulate_outbreak():
-    control_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                os.pardir, 'data',
-                                                'control.txt'))
     control_times, control_inc = np.loadtxt(control_path, unpack=True)
-
-    np.random.seed(2020)
 
     times, inc = simulate_outbreak(R0=1.5, k=1, n=1, D=10, gamma_shape=2,
                                    max_time=90, days_elapsed_max=50,
-                                   max_cases=1e4)
+                                   max_cases=1e4, seed=42)
 
     np.testing.assert_allclose(times, control_times)
     np.testing.assert_allclose(control_inc, inc)
+
+
+def print_result():
+
+    times, inc = simulate_outbreak(R0=1.5, k=1, n=1, D=10, gamma_shape=2,
+                                   max_time=90, days_elapsed_max=50,
+                                   max_cases=1e4, seed=42)
+
+    # np.savetxt(control_path, np.vstack([times, inc]).T)
+
+    print(times, inc)
