d0d7ad0fc95a7af7fd5fad71ac8f9f1d5397e455
Brett Morris <brettmorris21@gmail.com>
2020-02-29 09:49:17 +0100

    Adding unit test

--- eugene/tests/test_core.py
+++ eugene/tests/test_core.py
@@ -1,34 +1,19 @@
 import os
 import numpy as np
+import pytest
 
-from ..core import simulate_outbreak
+from ..core import sample_nbinom
 
 control_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                             os.pardir, 'data',
                                             'control.txt'))
 
-seed = 42
 
+@pytest.mark.parametrize("n, p", ((10, 0.5), (15, 0.1), (20, 0.01)))
+def test_nbinom(n, p):
+    np.random.seed(1)
+    nb_np = np.random.negative_binomial(n, p, size=10000)
+    nb_eu = sample_nbinom(n, p, size=10000)
 
-def test_simulate_outbreak():
-    control_times, control_inc = np.loadtxt(control_path, unpack=True)
-
-    times, inc = simulate_outbreak(R0=1.5, k=1, n=1, D=10, gamma_shape=2,
-                                   max_time=90, days_elapsed_max=50,
-                                   max_cases=1e4, seed=seed)
-
-    np.testing.assert_allclose(times, control_times)
-    np.testing.assert_allclose(control_inc, inc)
-
-
-def print_result():
-    """
-    python -c "from eugene.tests.test_core import print_result as p; p()"
-    """
-    times, inc = simulate_outbreak(R0=1.5, k=1, n=1, D=10, gamma_shape=2,
-                                   max_time=90, days_elapsed_max=50,
-                                   max_cases=1e4, seed=seed)
-
-    # np.savetxt(control_path, np.vstack([times, inc]).T)
-
-    print(times, inc)
+    assert (abs(nb_np.mean() - nb_eu.mean()) < 0.01 * n * (1 - p) / p)
+    assert (abs(nb_np.std() - nb_eu.std()) < 0.01 * n * (1 - p) / p)
