c35692037d9eea0cb13e4f4682d78e66a5ce06e1
Brett Morris <brettmorris21@gmail.com>
2020-03-11 12:43:59 +0100

    pre-flight updates

--- plot_grid.py
+++ plot_grid.py
@@ -34,17 +34,20 @@ if k_contour_plot:
     fig, ax = plt.subplots(figsize=(5, 4))
 
     X, Y = np.meshgrid(f_home_grid, k_grid)
-    Z = np.median(samples, axis=-1).T
-
-    # im = ax.pcolor(Y, X, k_grid,
-    #                cmap=plt.cm.Reds)
-    CS = ax.contour(X, Z, Y, levels=[0.1, 0.3, 0.5, 1.0, 3.0, 5.0])
-    ax.clabel(CS, inline=1, fontsize=10, fmt='$k = %.2f$')
+    Z = np.mean(samples, axis=-1).T
+    levels = [0.1, 0.2, 0.35, 0.58, 1.18]
+    CS = ax.contour(X, Z, Y, levels=levels, 
+                    colors=plt.cm.viridis((np.log10(levels) + 1) / 1.2))
+    ax.clabel(CS, inline=1, fontsize=10, fmt='$k = %.2f$', use_clabeltext=True)
     ax.set_yscale('log')
-    ax.set_ylim([1e-4, 1])
+    ax.set_yticks([1e-3, 1e-2, 1e-1, 0])
+    ax.set_yticklabels(['0.001', '0.01', '0.1', '1'])
+    ax.set_xticks(np.arange(0.5, 1.0, 0.05), minor=True)
+    ax.set_xticks(np.arange(0.5, 1.0, 0.1))
+    ax.set_ylim([3e-4, 1])
     # cbar = plt.colorbar(im, label='Final size')
 
     ax.set(ylabel='Final size', xlabel='$f_{\\rm home}$')
-    fig.savefig('plots/containment.pdf', bbox_inches='tight')
+    fig.savefig('plots/contour.pdf', bbox_inches='tight')
     plt.show()
 
